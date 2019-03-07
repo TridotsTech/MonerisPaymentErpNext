45a041402eb9e45feb4ea9d67db8d307dd9754bd
Your Name <you@example.com>
2019-03-07 19:30:37 +0530

    Payment From Sales order

--- moneris_payment/moneris_payment/doctype/moneris_settings/moneris_settings.py
+++ moneris_payment/moneris_payment/doctype/moneris_settings/moneris_settings.py
@@ -35,105 +35,112 @@ class MonerisSettings(Document):
 			self.reference_docname=data.get('payment_request_id')
 			payment_request=frappe.get_doc("Payment Request", data.get('payment_request_id'))
 			sale_order=frappe.get_doc("Sales Order",payment_request.reference_name)
-			billing_info=frappe.get_doc("Address", sale_order.customer_address)
-			shipping_info=frappe.get_doc("Address", sale_order.shipping_address_name)
-			# customer_info=frappe.get_doc("Customer", sale_order.customer)
-			
-			sale_order_items=frappe.db.get_all("Sales Order Item",  fields=['item_code,item_name,rate,qty'], filters={'parent':sale_order.name},limit_page_length=1000)
-			order_id =payment_request.reference_name
-			amount = data.get('payment_amount')
-			pan =data.get('card_number').replace(' ', '')
-			expiry_date = data.get('card_expire')
-			crypt = data.get('card_cvv')
-			customer=CustInfo()
+			if sale_order:
+				billing_info=frappe.get_doc("Address", sale_order.customer_address)
+				shipping_info=frappe.get_doc("Address", sale_order.shipping_address_name)
+				# customer_info=frappe.get_doc("Customer", sale_order.customer)
+				
+				sale_order_items=frappe.db.get_all("Sales Order Item",  fields=['item_code,item_name,rate,qty'], filters={'parent':sale_order.name},limit_page_length=1000)
+				order_id =payment_request.reference_name
+				print(sale_order.rounded_total)
+				amount = str(sale_order.rounded_total)
+				pan =data.get('card_number').replace(' ', '')
+				expiry_date = data.get('card_expire')
+				crypt = data.get('card_cvv')
+				customer=CustInfo()
 
-			#Customer Billing Info and Shipping Info
+				#Customer Billing Info and Shipping Info
 
-			first_name=sale_order.contact_display
-			last_name=" "
-			company_name=""
-			# Billing Info
-			billing_full_address=billing_info.address_line1
-			if billing_info.address_line2:
-				billing_full_address=billing_full_address+","+billing_info.address_line2
-			if len(billing_full_address)>70:
-				billing_full_address=billing_full_address[:70]
-			billing_address=billing_full_address
-			billing_city=billing_info.city
-			billing_state_province=billing_info.state
-			billing_postal_code=billing_info.pincode
-			billing_country=billing_info.country
-			billing_phone_number=billing_info.phone
-			billing_fax=""
-			billing_tax1=""
-			if sale_order.total_taxes_and_charges:
-				billing_tax1=str(sale_order.total_taxes_and_charges)
-			billing_tax2=""
-			billing_tax3=""
-			shipping_cost="0.00"
+				first_name=sale_order.contact_display
+				last_name=" "
+				company_name=""
+				# Billing Info
+				billing_full_address=billing_info.address_line1
+				if billing_info.address_line2:
+					billing_full_address=billing_full_address+","+billing_info.address_line2
+				if len(billing_full_address)>70:
+					billing_full_address=billing_full_address[:70]
+				billing_address=billing_full_address
+				billing_city=billing_info.city
+				billing_state_province=billing_info.state
+				billing_postal_code=billing_info.pincode
+				billing_country=billing_info.country
+				billing_phone_number=billing_info.phone
+				billing_fax=""
+				billing_tax1=""
+				if sale_order.total_taxes_and_charges:
+					billing_tax1=str(sale_order.total_taxes_and_charges)
+				billing_tax2=""
+				billing_tax3=""
+				shipping_cost="0.00"
 
-			# # Shipping Info
-			shipping_full_address=shipping_info.address_line1
-			if shipping_info.address_line2:
-				shipping_full_address=shipping_full_address+","+shipping_info.address_line2
-			if len(shipping_full_address)>70:
-				shipping_full_address=shipping_full_address[:70]
-			shipping_address=shipping_full_address
-			shipping_city=shipping_info.city
-			shipping_state_province=shipping_info.state
-			shipping_postal_code=shipping_info.pincode
-			shipping_country=shipping_info.country
-			shipping_phone_number=shipping_info.phone
-			shipping_fax=""
-			shipping_tax1=""
-			if sale_order.total_taxes_and_charges:
-				shipping_tax1=str(sale_order.total_taxes_and_charges)
-			shipping_tax2=""
-			shipping_tax3=""
-			shipping_cost="0.00"
-			billing_obj=BillingInfo(first_name, last_name, company_name, billing_address, billing_city, billing_state_province, billing_postal_code, billing_country, billing_phone_number, billing_fax, billing_tax1, billing_tax2, billing_tax3, shipping_cost)
-			shpping_obj=ShippingInfo(first_name, last_name, company_name, shipping_address, shipping_city, shipping_state_province, shipping_postal_code, shipping_country, shipping_phone_number, shipping_fax, shipping_tax1, shipping_tax2, shipping_tax3, shipping_cost)
-			customer.setBilling(billing_obj)
-			customer.setShipping(shpping_obj)
-			customer.setEmail(frappe.session.user)
-			customer.setInstruction(" ")
-			
-			# Product Items
+				# # Shipping Info
+				shipping_full_address=shipping_info.address_line1
+				if shipping_info.address_line2:
+					shipping_full_address=shipping_full_address+","+shipping_info.address_line2
+				if len(shipping_full_address)>70:
+					shipping_full_address=shipping_full_address[:70]
+				shipping_address=shipping_full_address
+				shipping_city=shipping_info.city
+				shipping_state_province=shipping_info.state
+				shipping_postal_code=shipping_info.pincode
+				shipping_country=shipping_info.country
+				shipping_phone_number=shipping_info.phone
+				shipping_fax=""
+				shipping_tax1=""
+				if sale_order.total_taxes_and_charges:
+					shipping_tax1=str(sale_order.total_taxes_and_charges)
+				shipping_tax2=""
+				shipping_tax3=""
+				shipping_cost="0.00"
+				billing_obj=BillingInfo(first_name, last_name, company_name, billing_address, billing_city, billing_state_province, billing_postal_code, billing_country, billing_phone_number, billing_fax, billing_tax1, billing_tax2, billing_tax3, shipping_cost)
+				shpping_obj=ShippingInfo(first_name, last_name, company_name, shipping_address, shipping_city, shipping_state_province, shipping_postal_code, shipping_country, shipping_phone_number, shipping_fax, shipping_tax1, shipping_tax2, shipping_tax3, shipping_cost)
+				customer.setBilling(billing_obj)
+				customer.setShipping(shpping_obj)
+				customer.setEmail(frappe.session.user)
+				customer.setInstruction(" ")
+				
+				# Product Items
 
-			for item in sale_order_items:
-				customer.addItem(Item(item.item_name[:45],str(item.qty),item.item_code.split(':')[0],str(item.rate)))
+				for item in sale_order_items:
+					customer.addItem(Item(item.item_name[:45],str(item.qty),item.item_code.split(':')[0],str(item.rate)))
 
 
-			#Purchase 
+				#Purchase 
 
-			purchase = Purchase(order_id, amount , pan, expiry_date, crypt)
-			purchase.setCustInfo(customer)
-			MSGObject=mpgHttpsPost(purchase)
-			MSGObject.postRequest()
-			
-			#Response
-			resp = MSGObject.getResponse()
-			if resp.getComplete()=="true":
-				self.integration_request.db_set('status', 'Completed', update_modified=False)
-				self.flags.status_changed_to = "Completed"
-				return self.finalize_request(data.get('payment_request_id'),str(resp.getTransID()),str(resp.getTransDate()))
+				purchase = Purchase(order_id, amount , pan, expiry_date, crypt)
+				purchase.setCustInfo(customer)
+				MSGObject=mpgHttpsPost(purchase)
+				MSGObject.postRequest()
+				
+				#Response
+				resp = MSGObject.getResponse()
+				if resp.getComplete()=="true":
+					self.integration_request.db_set('status', 'Completed', update_modified=False)
+					self.flags.status_changed_to = "Completed"
+					return self.finalize_request(data.get('payment_request_id'),str(resp.getTransID()),str(resp.getTransDate()))
+				else:
+					return {"ReceiptId" : resp.getReceiptId(),
+						"ReferenceNum" :resp.getReferenceNum(),
+						"ResponseCod" : resp.getResponseCode(),
+						"AuthCode" : resp.getAuthCode(),
+						"TransTime" : resp.getTransTime(),
+						"TransDate" : resp.getTransDate(),
+						"TransType" : resp.getTransType(),
+						"Complete" : resp.getComplete(),
+						"status" : resp.getComplete(),
+						"Message" : resp.getMessage(),
+						"TransAmount" : resp.getTransAmount(),
+						"CardType" : resp.getCardType(),
+						"TransID" : resp.getTransID(),
+						"TimedOut" : resp.getTimedOut(),
+						"BankTotals" : resp.getBankTotals(),
+						"Ticket" : resp.getTicket()}
 			else:
-				return {"ReceiptId" : resp.getReceiptId(),
-					"ReferenceNum" :resp.getReferenceNum(),
-					"ResponseCod" : resp.getResponseCode(),
-					"AuthCode" : resp.getAuthCode(),
-					"TransTime" : resp.getTransTime(),
-					"TransDate" : resp.getTransDate(),
-					"TransType" : resp.getTransType(),
-					"Complete" : resp.getComplete(),
-					"status" : resp.getComplete(),
-					"Message" : resp.getMessage(),
-					"TransAmount" : resp.getTransAmount(),
-					"CardType" : resp.getCardType(),
-					"TransID" : resp.getTransID(),
-					"TimedOut" : resp.getTimedOut(),
-					"BankTotals" : resp.getBankTotals(),
-					"Ticket" : resp.getTicket()}
+				return {
+						"redirect_to": "payment-failed",
+						"status": "failed"
+					}
 		except Exception,e:
 			print(e)
 			return e
