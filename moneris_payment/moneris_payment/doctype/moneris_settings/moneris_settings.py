af339afe06d8389c2648afde59622fbe27eabc48
Your Name <you@example.com>
2019-03-30 11:58:02 +0530

    Integration

--- moneris_payment/moneris_payment/doctype/moneris_settings/moneris_settings.py
+++ moneris_payment/moneris_payment/doctype/moneris_settings/moneris_settings.py
@@ -27,7 +27,7 @@ class MonerisSettings(Document):
 
 	def create_request(self, data):
 		try:
-			from moneris_payment.MonerisPaymentGateway  import Vault,PurchaseWithVault,mpgHttpsPost,CustInfo
+			from moneris_payment.MonerisPaymentGateway  import Vault,PurchaseWithVault,mpgHttpsPost,CustInfo,BillingInfo,ShippingInfo
 			self.data= json.loads(data)
 			self.integration_request = create_request_log(self.data, "Host", "Moneris")
 			data = json.loads(data)
